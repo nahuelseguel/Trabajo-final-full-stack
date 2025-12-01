b6c90cf6323bdd7ce9a5d877f287f83b6240bfa5
altieriagustina <altieriagustina@gmail.com>
2025-12-01 20:54:39 -0300

    Agrego rutas login

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { PantallaCliente } from './components/seccionClientes/PantallaCliente'
 import { PantallaProfesionales } from './components/CardProfesionales/PantallaProfesionales'
-
-import { Route, Routes} from 'react-router-dom'
+import { Login } from "./Screens/Login";
+import { ForgotPassword } from "./Screens/ForgotPassword"
+import { SignUp } from "./Screens/SignUp"
+import { Route, Routes } from 'react-router-dom'
 import { Turnos } from './components/Turnos'
 
 function App() {
 
 
-//cree una url dinamica, :nombre va a cambiar segun la card que elije
+  //cree una url dinamica, :nombre va a cambiar segun la card que elije
   return (
     <>
       <Routes>
         <Route path="/turnos" element={<Turnos></Turnos>}></Route>
-       <Route path="/" element={<PantallaCliente></PantallaCliente>}></Route> 
-       <Route path="/profesion/:profesion" element={<PantallaProfesionales/>}></Route>
+        <Route path="/" element={<PantallaCliente></PantallaCliente>}></Route>
+        <Route path="/profesion/:profesion" element={<PantallaProfesionales />}></Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   )

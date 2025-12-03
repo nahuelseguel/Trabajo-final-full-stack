b926a2a3137c49b3c2a870cd863c5704c1aa95a0
NahuelSeguel <seguelnahuel@gmail.com>
2025-12-03 20:09:23 -0300

    se crearon componentes calendario, horarios y confirmacion con respectivos css

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
+import Calendario from "./components/calendario/Calendario";
+import Horarios from "./components/horarios/Horarios";
+import Confirmacion from "./components/confirmacion/Confirmacion";
 import { PantallaCliente } from './components/seccionClientes/PantallaCliente'
 import { PantallaProfesionales } from './components/CardProfesionales/PantallaProfesionales'
-import { Login } from "./Screens/Login";
-import { ForgotPassword } from "./Screens/ForgotPassword"
-import { SignUp } from "./Screens/SignUp"
+// import { Login } from "./Screens/Login";
+// import { ForgotPassword } from "./Screens/ForgotPassword"
+// import { SignUp } from "./Screens/SignUp"
 import { Route, Routes } from 'react-router-dom'
 import { Turnos } from './components/Turnos'
+import { useState } from 'react'
 
 function App() {
 
-
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTime, setSelectedTime] = useState(null);
   //cree una url dinamica, :nombre va a cambiar segun la card que elije
   return (
     <>
       <Routes>
+        <Route path="/calendario" element={<Calendario onDateSelect={(date) => setSelectedDate(date)} />} />
+        <Route path="/horarios" element={<Horarios selectedDate={selectedDate} onTimeSelect={(time) => setSelectedTime(time)} />} />
+        <Route path="/confirmacion" element={<Confirmacion finalSelection={{ date: selectedDate, time: selectedTime }} />} />
         <Route path="/turnos" element={<Turnos></Turnos>}></Route>
         <Route path="/" element={<PantallaCliente></PantallaCliente>}></Route>
         <Route path="/profesion/:profesion" element={<PantallaProfesionales />}></Route>
-        <Route path="/" element={<Login />} />
+        {/* <Route path="/" element={<Login />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUp />} /> */}
       </Routes>
     </>
   )

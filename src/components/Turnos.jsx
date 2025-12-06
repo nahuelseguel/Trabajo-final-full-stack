384e1955b7ba6af4359362d57e46c4208d87715d
braianCaviggia <braiancaviggia10@gmail.com>
2025-12-05 21:17:47 -0300

    Agregue notificacion de turnos solicidatos - lado cliente

--- src/components/Turnos.jsx
+++ src/components/Turnos.jsx
@@ -3,13 +3,42 @@ import { useLocation } from 'react-router-dom'
 
 export const Turnos = () => {
 
-    const location = useLocation()
-    const profesional = location.state
+    const [solicitudesTurnos, setSolicitudesTurnos] = useState([]);
+    const [turnoAceptado, setTurnoAceptado] = useState([]);
+    const [turnosRechazados, setTurnosRechazados] = useState([]);
+
+    const cliente = JSON.parse(localStorage.getItem("usuario"));
+    const idCliente = ciente?.id
+
+    const [turnos, setTurnos] = useState([])
+
+    useEffect(() => {
+        const traerTurnos = async () => {
+            const res = await fetch("http://localhost:4000/turnos");
+            const data = await res.json();
+
+            // comparo el id del profesional con el id del turno para que coincida y se le muestre a la persona que corresponde
+            const misTurnos = data.filter(turno => turno.clienteId === idCliente);
+
+            setSolicitudesTurnos(misTurnos.filter(t => t.estado === "pendiente"))
+            setTurnoAceptado(misTurnos.filter(t => t.estado === "aceptado"))
+            setTurnosRechazados(misTurnos.filter(t => t.estado === "rechazado"))            
+        };
+
+        traerTurnos();
+    }, [idCliente]);
+
 
 
     return (
         <div>
-            <h2>Turnos disponibles de {profesional.nombre}</h2>
+            <h2>Mis turnos</h2>
+
+            {solicitudesTurnos.map(s => (
+                <ul>
+                    <li>{s}</li>
+                </ul>
+            ))}
         </div>
     )
 }
--- src/components/seccionClientes/PantallaCliente.jsx
+++ src/components/seccionClientes/PantallaCliente.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card } from "./Card/Card.jsx"
 import profesiones from "./Card/profesiones.json"
 import "../seccionClientes/pantallaPrincipal.css"
@@ -6,6 +6,39 @@ import "../seccionClientes/pantallaPrincipal.css"
 //el boton buscar no tiene accion 
 
 export const PantallaCliente = () => {
+
+
+    const [solicitudesTurnos, setSolicitudesTurnos] = useState([]);
+    const [turnoAceptado, setTurnoAceptado] = useState([]);
+    const [turnosRechazados, setTurnosRechazados] = useState([]);
+
+    const cliente = JSON.parse(localStorage.getItem("usuario"));
+    const idCliente = cliente?.id
+
+    useEffect(() => {
+        const traerTurnos = async () => {
+            const res = await fetch("http://localhost:4000/turnos");
+            const data = await res.json();
+
+            const misTurnos = data.filter(turno => turno.clienteId === idCliente);
+
+            const usuarios = await fetch("http://localhost:4000/usuarios");
+            const listaUsuarios = await usuarios.json();
+
+            const turnosProfesional = misTurnos.map(t => {
+                const profesional = listaUsuarios.find(u => u.id === t.profesionalId)
+                return { ...t, profesional };
+            })
+
+
+            setSolicitudesTurnos(turnosProfesional.filter(t => t.estado === "pendiente"))
+            setTurnoAceptado(turnosProfesional.filter(t => t.estado === "confirmado"))
+            setTurnosRechazados(turnosProfesional.filter(t => t.estado === "rechazado"))
+        };
+
+        traerTurnos();
+    }, [idCliente]);
+
 
     const [busqueda, setBusqueda] = useState("")
 
@@ -29,6 +62,32 @@ export const PantallaCliente = () => {
                     {profesionesFiltradas.map((p) => (
                         <Card imagen={p.imagen} profesion={p.profesion}></Card>
                     ))}
+                </div>
+
+                <div className='contenedor-turnos'>
+                    <h2 className='texto-turnos'>Mis turnos</h2>
+
+                    {solicitudesTurnos.map(s => (
+                        <ul>
+                            <li>Turno pedido a {s.profesional?.nombre} {s.profesional?.apellido} para el dia {s.fecha} a las {s.horario} se encuentra {s.estado}</li>
+                          
+                        </ul>
+                    ))}
+
+                    {turnoAceptado.map(s => (
+                        <ul>
+                            <li>Turno pedido a {s.profesional?.nombre} {s.profesional?.apellido} para el dia {s.fecha} a las {s.horario} se encuentra {s.estado}</li>
+                        </ul>
+                    ))}
+
+                    {turnosRechazados.map(s => (
+                        <ul>
+                            <li>Turno pedido a {s.profesional?.nombre} {s.profesional?.apellido} para el dia {s.fecha} a las {s.horario} se encuentra {s.estado}</li>
+                        </ul>
+                    ))}
+
+                      <button className='btn-vaciar' onClick={() => setTurnoAceptado([])}>Eliminar notificaciones de turnos aceptados</button>
+                      <button className='btn-vaciar' onClick={() => setTurnosRechazados([])}>Eliminar notificaciones de turnos rechazados</button>
                 </div>
             </div>
         </>
